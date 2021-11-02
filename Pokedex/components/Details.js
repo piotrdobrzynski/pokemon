840bad50d945a4e9fb7ac6a416eae1ebe6653307
coldoe <hawxhawx1920@gmail.com>
2021-11-02 15:59:12 +0100

    retrieve data from api

---
+++ Pokedex/components/Details.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, Image, StyleSheet, ActivityIndicator} from 'react-native';
+
+const Details = props => {
+  const [details, setDetails] = useState([]);
+
+  useEffect(() => {
+    fetchPokemonDetails();
+  }, []);
+
+  const fetchPokemonDetails = () => {
+    const {state} = props.navigation;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${state.params.pokemon}`)
+      .then(res => res.json())
+      .then(details => setDetails(details));
+  };
+}
