fafdbf4c478ffb14f8c41214643f37aade93515e
Piotr Dobrzyński <50482622+piotrdobrzynski@users.noreply.github.com>
2022-01-18 18:20:56 +0100

    Update Pokemons.js

--- Pokedex/components/Pokemons.js
+++ Pokedex/components/Pokemons.js
@@ -42,7 +42,7 @@ const Pokemons = (props) => {
                           pokemon: pokemon.name,
                         })
                       }>
-                      <Image
+                      <FastImage
                         style={{width: 150, height: 150}}
                         source={{
                           uri: `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${
@@ -63,19 +63,34 @@ export default Pokemons;
 
 
 const styles = StyleSheet.create({  
-    container: {    
-     display: 'flex',    
-     flexDirection: 'row',    
-     flexWrap: 'wrap',    
-     justifyContent: 'center',    
-     marginTop: 30,  
-    },  
-    card: {    
-     display: 'flex',    
-     alignItems: 'center',    
-     borderBottomWidth: 1,    
-     borderBottomColor: 'black',    
-     marginHorizontal: 20,    
-     marginVertical: 10,  
-    },  
-    });
+  container: {    
+   display: 'flex',    
+   flexDirection: 'row',    
+   flexWrap: 'wrap',    
+   justifyContent: 'center',    
+   marginTop: 30,  
+  },  
+  card: {    
+   display: 'flex',    
+   alignItems: 'center',    
+   borderBottomWidth: 1,    
+   borderBottomColor: 'black',    
+   marginHorizontal: 20,    
+   marginVertical: 10,  
+  },  
+  searchCont: {    
+   position: 'absolute',    
+   marginBottom: 70,    
+   left: '20%',    
+   zIndex: 1,    
+   marginTop: 10,  
+  },  
+  searchfeild: {    
+  height: 40,    
+  borderWidth: 1,    
+  borderColor: '#000',    
+  textAlign: 'center',    
+  width: 250,    
+  borderRadius: 50,  
+  },
+  });
