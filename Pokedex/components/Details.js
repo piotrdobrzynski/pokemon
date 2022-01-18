f300ecb8fbae02b19261708e7787ee6facf8ed92
coldoe <hawxhawx1920@gmail.com>
2022-01-18 16:25:10 +0100

    caching images

--- Pokedex/components/Details.js
+++ Pokedex/components/Details.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, Image, StyleSheet, ActivityIndicator} from 'react-native';
+import FastImage from 'react-native-fast-image';
 
 const Details = props => {
   const [details, setDetails] = useState([]);
@@ -16,18 +17,29 @@ const Details = props => {
   };
   return details.name ? (
     <View style={{flex: 1, alignItems: 'center'}}>
-      <Image
+      <FastImage
         style={styles.image}
         source={{
           uri: `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${
             details.name
           }.png`,
+          priority: FastImage.priority.normal,
         }}
       />
 
       <Text style={styles.text}>Name: {details.name}</Text>
       <Text style={styles.text}>Height: {details.height}</Text>
       <Text style={styles.text}>Weight: {details.weight}</Text>
+
+      <Text style={styles.text}>Ability 1: {
+        typeof details.abilities[0].ability.name  !== 'undefined' || details.abilities[0].ability.name === null ?
+        details.abilities[0].ability.name : 'none'
+      } </Text>
+
+      <Text style={styles.text}>Ability 2: {
+      typeof details.abilities[1].ability.name  !== 'undefined' || details.abilities[1].ability.name === null ?
+      details.abilities[1].ability.name : 'none'
+      } </Text>
 
     </View>
   ) : (
